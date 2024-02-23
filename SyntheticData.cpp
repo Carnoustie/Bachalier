16c3a0aa157c952565739f6348ab954a1a36c415
osp <osp@kth.se>
2024-02-23 05:48:28 +0100

    Remove the nested Eigen repository

---
+++ SyntheticData.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <gtkmm.h>
+#include <cairomm/context.h>
+#include <vector>
+#include <random>
+#include <algorithm>
+
+#include "Plot.hpp"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <thread>
+
+
+using namespace std;
+using namespace Gtk;
+
+vector<coord>* ItoProccess(int dim, double timeSpan, double stepSize){
+
+}
---
+++ SyntheticData.hpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <gtkmm.h>
+#include <cairomm/context.h>
+#include <vector>
+#include <random>
+#include <algorithm>
+
+#include "Plot.hpp"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <thread>
+
+
+using namespace std;
+using namespace Gtk;
+
