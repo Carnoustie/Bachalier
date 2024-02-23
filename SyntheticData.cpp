6bec1938edaf168c12fa4ba05756fe3556060bf9
osp <osp@kth.se>
2024-02-23 10:10:05 +0100

    Work on generating interdependent multi-asset price paths

--- SyntheticData.cpp
+++ SyntheticData.cpp
@@ -27,11 +27,40 @@ random_device rd{}; //True number generator from hardware noise
 mt19937 gen{rd()}; //Mersenne twister to output Uniform distribution given random integers from rd.
 normal_distribution<> nd{0.0, 1.0}; //Gaussian number generator from Inverse Sampling using Uniform distribution
 
-/*
-vector<coord>* ItoProccess(int dim, double timeSpan, double stepSize){
 
+double** ItoProccess(int dim, double timeSpan, double timeStep, Eigen::VectorXd mu, Eigen::MatrixXd sigma){
+    double** paths;
+    
+
+    int N = timeSpan/timeStep;
+    
+    double priorY = 0.0;
+    double nextY;
+
+    double priorX = 0.0;
+    double nextX;
+    
+    Eigen::VectorXd gaussianIncrement;
+    double rootStep = sqrt(timeStep);
+    Eigen::VectorXd gaussianIncrement;
+    for(int k=0; k<N; k++){
+        nextX = priorX + timeStep;
+        priorX = nextX;
+
+        gaussianIncrement = GaussianVector(dim, 0, 1);
+        nextY = priorY + timeStep*mu + rootStep*sigma*gaussianIncrement;
+        priorY = nextY;
+
+
+        currentPoint.x = nextX;
+        currentPoint.y = nextY;
+
+        path[k] = currentPoint;
+    }
+
+    return path;
 }
-*/
+
 
 
 
@@ -46,10 +75,26 @@ Eigen::VectorXd GaussianVector(int dim, double mu, double sigma){
     return gv;
 }
 
+Eigen::MatrixXd GaussianMatrix(int rows, int cols, double mu, double sigma){
+    Eigen::MatrixXd gm(rows, cols);
+    for(int k=0; k<rows; k++){
+        for(int i=0; i<cols; i++){
+            gm(k,i)= mu + sigma*nd(gen);
+        }
+    }
+    return gm;
+}
+
 
 
 int main(int argc, char * argv[]){
 
     Eigen::VectorXd gv = GaussianVector(5, 2, 5);
-    cout <<"\n\n" <<  gv << "\n\n";
+//    cout <<"\n\n" <<  gv << "\n\n";
+    Eigen::MatrixXd gm = GaussianMatrix(3,3, 0, 1);
+    Eigen::MatrixXd cov = (gm + gm.transpose())/2; //example for covariance matrix to test other features
+    cout <<"\n\n" <<  cov << "\n\n";
+    Eigen::LLT<Eigen::MatrixXd> covLLT = cov.llt();
+    Eigen::MatrixXd covRoot = covLLT.matrixL();
+
 }
