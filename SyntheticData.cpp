343a7ac8d7e010402c15ee68566afd0c24700345
osp <osp@kth.se>
2024-02-25 09:27:35 +0100

    Support for Ito vectors with custom covariance matrix

--- SyntheticData.cpp
+++ SyntheticData.cpp
@@ -26,42 +26,7 @@ using namespace Gtk;
 random_device rd{}; //True number generator from hardware noise
 mt19937 gen{rd()}; //Mersenne twister to output Uniform distribution given random integers from rd.
 normal_distribution<> nd{0.0, 1.0}; //Gaussian number generator from Inverse Sampling using Uniform distribution
-
-
-double** ItoProccess(int dim, double timeSpan, double timeStep, Eigen::VectorXd mu, Eigen::MatrixXd sigma){
-    double** paths;
-    
-
-    int N = timeSpan/timeStep;
-    
-    double priorY = 0.0;
-    double nextY;
-
-    double priorX = 0.0;
-    double nextX;
-    
-    Eigen::VectorXd gaussianIncrement;
-    double rootStep = sqrt(timeStep);
-    Eigen::VectorXd gaussianIncrement;
-    for(int k=0; k<N; k++){
-        nextX = priorX + timeStep;
-        priorX = nextX;
-
-        gaussianIncrement = GaussianVector(dim, 0, 1);
-        nextY = priorY + timeStep*mu + rootStep*sigma*gaussianIncrement;
-        priorY = nextY;
-
-
-        currentPoint.x = nextX;
-        currentPoint.y = nextY;
-
-        path[k] = currentPoint;
-    }
-
-    return path;
-}
-
-
+vector<coord> examplePath(800);
 
 
 //Independent Increments
@@ -74,6 +39,60 @@ Eigen::VectorXd GaussianVector(int dim, double mu, double sigma){
     }
     return gv;
 }
+
+
+Eigen::VectorXd ItoProccess(int dim, double timeSpan, double timeStep, Eigen::VectorXd mu, Eigen::MatrixXd sigma){
+    //double paths[dim];
+    
+
+    int N = timeSpan/timeStep;
+    Eigen::VectorXd priorY = Eigen::VectorXd::Zero(dim);
+    Eigen::VectorXd nextY;
+
+    Eigen::VectorXd priorX = Eigen::VectorXd::Zero(dim);
+    Eigen::VectorXd nextX;
+    Eigen::VectorXd stepX = timeStep*Eigen::VectorXd::Ones(dim);
+    
+    Eigen::VectorXd gaussianIncrement;
+    double rootStep = sqrt(timeStep);
+    
+    cout << "\n\nN: " << N << "\n\n";
+    
+    coord forTest;
+
+    for(int k=0; k<N; k++){
+
+
+        nextX = priorX + stepX;
+        priorX = nextX;
+    
+        gaussianIncrement = GaussianVector(dim, 0, 1);
+        nextY = priorY + timeStep*mu + rootStep*sigma*gaussianIncrement;
+        priorY = nextY;
+        
+        forTest.x = nextX(4);
+        forTest.y = nextY(4);
+
+        cout <<"\n\nx: " << forTest.x  << "\n\n"; 
+        cout <<"\n\ny: " <<  forTest.y <<"\n\n"; 
+        examplePath[k] = forTest;
+
+
+        //currentPoint.x = nextX;
+        //currentPoint.y = nextY;
+
+        //paths[k] = currentPoint;
+    }
+    
+
+
+
+    return nextY;
+}
+
+
+
+
 
 Eigen::MatrixXd GaussianMatrix(int rows, int cols, double mu, double sigma){
     Eigen::MatrixXd gm(rows, cols);
@@ -91,10 +110,31 @@ int main(int argc, char * argv[]){
 
     Eigen::VectorXd gv = GaussianVector(5, 2, 5);
 //    cout <<"\n\n" <<  gv << "\n\n";
-    Eigen::MatrixXd gm = GaussianMatrix(3,3, 0, 1);
+    Eigen::MatrixXd gm = GaussianMatrix(5,5, 0, 1);
     Eigen::MatrixXd cov = (gm + gm.transpose())/2; //example for covariance matrix to test other features
     cout <<"\n\n" <<  cov << "\n\n";
     Eigen::LLT<Eigen::MatrixXd> covLLT = cov.llt();
     Eigen::MatrixXd covRoot = covLLT.matrixL();
+
+    Eigen::VectorXd mu = Eigen::VectorXd::Zero(5);
+
+    Eigen::VectorXd samplePrices = ItoProccess(5, 800, 1, mu, covRoot);
+    cout << "\n\n" << samplePrices << "\n\n";
+
+
+
+    auto app = Application::create();
+    Window w;
+    w.set_default_size(1600,1360);
+    w.move(2000,0);
+    Plot p;
+    
+    p.addPlot(&examplePath);
+    
+    w.add(p);
+    w.show_all();
+    w.set_title("Asset Price Dynamics");
+    app->run(w);
+
 
 }
